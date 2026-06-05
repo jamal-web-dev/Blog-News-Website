7cd820cf5d0ee14c8aad69f21822e626b95cb0a5
Jamal <jamaly381@gmail.com>
2026-06-05 21:36:40 +0100

    Dynamic Header

--- src/main.js
+++ src/main.js
@@ -1,6 +1,21 @@
-const navIcon = document.querySelector(".nav-icon");
-const navMenu = document.querySelector(".nav-menu");
 
-navIcon.addEventListener("click", ()=>{
-  navMenu.classList.toggle("active");
-})
+// ============ DYNAMIC HEADER=====
+
+async function loadHeader() {
+  try {
+    const response = await fetch("../header.html");
+    const data = await response.text();
+    const header = document.querySelector("header");
+    header.innerHTML = data;
+
+    const navIcon = document.querySelector(".nav-icon");
+    const navMenu = document.querySelector(".nav-menu");
+
+    navIcon.addEventListener("click", ()=>{
+      navMenu.classList.toggle("active");
+    })
+  }catch (error) {
+    console.log(error)
+  }
+}
+loadHeader();
